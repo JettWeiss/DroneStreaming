9902188a310b41f4e0f9c191002810e3a10f418a
JettWeiss <jettxavierweiss@gmail.com>
2026-08-19 17:33:58 -0400

    2026-08-19 shows photos

--- testing/websocket.py
+++ testing/websocket.py
@@ -1,11 +1,20 @@
 import asyncio
 from websockets.asyncio.server import serve
+from PIL import Image
+import io
+
 async def handler(websocket):
+    print("connected")
     async for message in websocket:
-        print(message)
+        print("received")
+        if isinstance(message, bytes):
+            print("is image")
+            Image.open(io.BytesIO(message)).show()
+        else:
+            print(message)
 
 async def main():
-    server = await serve(handler, "0.0.0.0", 8765)
+    server = await serve(handler, "0.0.0.0", 8765, max_size=10*1024*1024)
     await server.serve_forever()
 
 asyncio.run(main())
